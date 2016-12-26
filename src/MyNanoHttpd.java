58638e53ca8270914c65e46ac8706a3168142adf
fengwutianya <949979029@qq.com>
2016-12-26 17:03:23 +0800

    Initial commit

---
+++ src/MyNanoHttpd.java
@@ -0,0 +1,10 @@
+/**
+ * Created by xuan on 2016/12/26 0026.
+ */
+public class MyNanoHttpd {
+    public static void main(String[] args) {
+        //default port and www root
+        int port = 80;
+        
+    }
+}
